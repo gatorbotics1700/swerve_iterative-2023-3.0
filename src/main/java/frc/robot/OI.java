064579d796ecddf7410fc05db2e830891c083d6b
25laurenj <25ljene@castilleja.org>
2023-01-17 19:19:21 -0800

    telescopingarm sensor, gain, deadband and buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,4 +7,7 @@ public class OI {
     public static final XboxController m_controller = new XboxController(0);
 
     public static Button stop = Button.kB;
+    public static Button armExtended  = Button.kY;//button to fully extend to 90 degrees
+    public static Button swingThrough  = Button.kX;//button to go through
+    public static Button armControl  = Button.kLeftStick;//stick to control arm length (any length from fully retracted to extended)
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import frc.robot.Constants;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;//written as import com.ctre.phoenix.motorcontrol.TalonFXControlMode; in some documentation. is ours okay??
 import edu.wpi.first.math.controller.PIDController;
 
 public class ArmSubsystem {
@@ -15,6 +15,8 @@ public class ArmSubsystem {
 
     TalonFX armMotor = new TalonFX(Constants.ARM_MOTOR_ID);
 
+    armMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, Constants.kPIDLoopIdx, Constants.kTimeoutMs);//determine what these values would be for us
+
     public ArmSubsystem(){
 
     }
@@ -23,12 +25,17 @@ public class ArmSubsystem {
         return armMotor.getSelectedSensorPosition();
     }
 
-    public void armPID(double armLengthSetpoint){
-        armLengthController.setSetpoint(armLengthSetpoint);
-        double output = armLengthController.calculate(getArmLength(), armLengthSetpoint);
-        System.out.println("output: " + output);
-        armMotor.set(ControlMode.PercentOutput, output);
+    if (Math.abs(leftYstick) < 0.10/*this number should be changed through testing*/) {
+        /* Within 10% of zero */
+        leftYstick = 0;//left joystick y axis in documentation. assign later
     }
+
+    //public void armPID(double armLengthSetpoint){
+       // armLengthController.setSetpoint(armLengthSetpoint);
+        //double output = armLengthController.calculate(getArmLength(), armLengthSetpoint);
+        //System.out.println("output: " + output);
+        //armMotor.set(ControlMode.PercentOutput, output);//written as _talon.set(TalonFXControlMode.PercentOutput, leftYstick); in documentation. leftystick is joy.gety
+    //}
 
     public void telescoping(double armSetpoint){//setpoint in inches
         armSetpoint = armSetpoint * Constants.TICKS_PER_INCH;//will probably have to change because wheel diameter in this constant is not relevant
