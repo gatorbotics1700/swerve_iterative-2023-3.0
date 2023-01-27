ffcb550cf673ca57d706215ffc4217d52e2b3ed2
24jzhao <24jzhao@castilleja.org>
2023-01-27 15:41:04 -0800

    april tag locations

---
+++ src/main/java/frc/robot/subsystems/AprilTagLocation.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+public class AprilTagLocation{
+    Pose3d idOne = new Pose3d(Translation3d(15.513558, 1.071626, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
+    Pose3d idTwo = new Pose3d(Translation3d(15.513558, 2.748026, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
+    Pose3d idThree = new Pose3d(Translation3d(15.513558, 4.424426, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
+    Pose3d idFour = new Pose3d(Translation3d(16.178784, 6.749796, 0.695452), Rotation3d(0.0, 0.0, 0.0, 1.0));
+    Pose3d idFive = new Pose3d(Translation3d(0.36195, 6.749796, 0.695452), Rotation3d(1.0, 0.0, 0.0, 0.0));
+    Pose3d idSix = new Pose3d(Translation3d(1.02743, 4.424426, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
+    Pose3d idSeven = new Pose3d(Translation3d(1.02743, 2.748026, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
+    Pose3d idEight = new Pose3d(Translation3d(1.02743, 1.071626, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
+}
