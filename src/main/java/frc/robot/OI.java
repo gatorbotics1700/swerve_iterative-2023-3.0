faa1af18e6dbc8ecd632392e5276e1538b2b7b2c
Kaylin Yip <kaylinyip@Kaylins-MacBook-Air.local>
2023-01-26 18:41:32 -0800

    added notes for xbox controller buttons and triggers

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,12 +10,25 @@ public class OI {
     public static Button stop = Button.kB;
     public static Button turnNorth = Button.kA;//button to turn to true north
     public static Button armExtended  = Button.kY;//button to fully extend to 90 degrees
-    public static Button swingThrough  = Button.kX;//button to go through
+    public static Button swingThrough  = Button.kX;//button to go through don't know if we need this
     public static Button armControl  = Button.kLeftStick;//stick to control arm length (any length from fully retracted to extended)
 
     public static Button intakeOn = Button.kA; // second xbox controller -- need to figure out how to map this to a certain xbox controller
 
     public static Button intakeOff = Button.kX; // second xbox controller
 
-    /*two total xbox controllers, one for driver, one for co-driver. 4 buttons on each, 2 joysticks, 2 triggers, 2 bumbers, 2 buttons at the back of the contoller, and possibly 4 directional arrow keys. All can be programmed for different settings as desired. */
+    /*two total xbox controllers, one for driver, one for co-driver. 4 buttons on each, 2 joysticks, 2 triggers, 2 bumbers, 2 additional buttons at the back of the contoller, and possibly 4 directional arrow keys. All can be programmed for different settings as desired.
+     * already coded buttons/joysticks:
+     * - true north
+     * - stop button
+     * - arm fully extended
+     * - swing through - not sure if necessary
+     * - arm control joystick
+     * - intake on and off
+     * buttons/joysticks to be coded:
+     * - make joystick for moving robot
+     * - states for elevovator - low, mid, high
+     * - pre-programmed/pre-set states for telescoping arm(maybe different shelf heights)
+     * - outtake button
+     */
 }
