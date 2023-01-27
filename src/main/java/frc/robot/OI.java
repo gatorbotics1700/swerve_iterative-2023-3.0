196889ec68aa27d86030923f26775434a407afcb
Kaylin Yip <kaylinyip@Kaylins-MacBook-Air.local>
2023-01-26 19:53:39 -0800

    added xbox buttons for various objectives

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,34 +1,32 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController.*;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
-    public static final XboxController m_controller = new XboxController(0);
-    public static final XboxController m_controller_two = new XboxController(1);
+    public static final XboxController m_controller = new XboxController(0);//buttons/co-driver controller
+    public static final XboxController m_controller_two = new XboxController(1);//main driver/driving controller
 
-    public static Button stop = Button.kB;
-    public static Button turnNorth = Button.kA;//button to turn to true north
-    public static Button armExtended  = Button.kY;//button to fully extend to 90 degrees
-    public static Button swingThrough  = Button.kX;//button to go through don't know if we need this
-    public static Button armControl  = Button.kLeftStick;//stick to control arm length (any length from fully retracted to extended)
+    //public static Button armExtended  = Button.kY;//button to fully extend to 90 degrees
+    //public static Button armControl  = Button.kLeftStick;//stick to control arm length (any length from fully retracted to extended)
+    public static Button pitchPD = Button.kLeftBumper;//pitch pd for charge station
+    public static Button stop = Button.kRightBumper;//stop drive bumper
+    //public static Trigger leftTrigger = Trigger.;
 
-    public static Button intakeOn = Button.kA; // second xbox controller -- need to figure out how to map this to a certain xbox controller
+    public static Button lowShelf = Button.kX; // low shelf height combo of elevator and arm angle
+    public static Button midShelf = Button.kA; // mid shelf
+    public static Button highShelf = Button.kB; // high shelf
+    public static Button turnNorth = Button.kY;//button to turn to true north
 
-    public static Button intakeOff = Button.kX; // second xbox controller
+
+    public static Button intakeOn = Button.kA; //need to be coded for triggers rather than buttons
+    public static Button intakeOff = Button.kX; // need to figure out code for triggers
+    //public static Button outtake = Button. - need a to change intake to triggers so we can use buttons for outtake
 
     /*two total xbox controllers, one for driver, one for co-driver. 4 buttons on each, 2 joysticks, 2 triggers, 2 bumbers, 2 additional buttons at the back of the contoller, and possibly 4 directional arrow keys. All can be programmed for different settings as desired.
-     * already coded buttons/joysticks:
-     * - true north
-     * - stop button
-     * - arm fully extended
-     * - swing through - not sure if necessary
-     * - arm control joystick
-     * - intake on and off
      * buttons/joysticks to be coded:
-     * - make joystick for moving robot
-     * - states for elevovator - low, mid, high
-     * - pre-programmed/pre-set states for telescoping arm(maybe different shelf heights)
+     * - make joystick for driving robot
      * - outtake button
      */
 }
