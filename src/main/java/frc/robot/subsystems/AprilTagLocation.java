e06a2facdad71089c55a95c95b3080ca9ede217f
dragonette13 <65913060+dragonette13@users.noreply.github.com>
2023-01-27 16:45:37 -0800

    fixed syntax errors in AprilTagLocation.java

--- src/main/java/frc/robot/subsystems/AprilTagLocation.java
+++ src/main/java/frc/robot/subsystems/AprilTagLocation.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Quaternion;
 
 public class AprilTagLocation{
-    Pose3d idOne = new Pose3d(Translation3d(15.513558, 1.071626, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
-    Pose3d idTwo = new Pose3d(Translation3d(15.513558, 2.748026, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
-    Pose3d idThree = new Pose3d(Translation3d(15.513558, 4.424426, 0.462788), Rotation3d(0.0, 0.0, 0.0, 1.0));
-    Pose3d idFour = new Pose3d(Translation3d(16.178784, 6.749796, 0.695452), Rotation3d(0.0, 0.0, 0.0, 1.0));
-    Pose3d idFive = new Pose3d(Translation3d(0.36195, 6.749796, 0.695452), Rotation3d(1.0, 0.0, 0.0, 0.0));
-    Pose3d idSix = new Pose3d(Translation3d(1.02743, 4.424426, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
-    Pose3d idSeven = new Pose3d(Translation3d(1.02743, 2.748026, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
-    Pose3d idEight = new Pose3d(Translation3d(1.02743, 1.071626, 0.462788), Rotation3d(1.0, 0.0, 0.0, 0.0));
+    Pose3d idOne = new Pose3d(new Translation3d(15.513558, 1.071626, 0.462788), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 1.0)));
+    Pose3d idTwo = new Pose3d(new Translation3d(15.513558, 2.748026, 0.462788), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 1.0)));
+    Pose3d idThree = new Pose3d(new Translation3d(15.513558, 4.424426, 0.462788), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 1.0)));
+    Pose3d idFour = new Pose3d(new Translation3d(16.178784, 6.749796, 0.695452), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 1.0)));
+    Pose3d idFive = new Pose3d(new Translation3d(0.36195, 6.749796, 0.695452), new Rotation3d(new Quaternion(1.0, 0.0, 0.0, 0.0)));
+    Pose3d idSix = new Pose3d(new Translation3d(1.02743, 4.424426, 0.462788), new Rotation3d(new Quaternion(1.0, 0.0, 0.0, 0.0)));
+    Pose3d idSeven = new Pose3d(new Translation3d(1.02743, 2.748026, 0.462788), new Rotation3d(new Quaternion(1.0, 0.0, 0.0, 0.0)));
+    Pose3d idEight = new Pose3d(new Translation3d(1.02743, 1.071626, 0.462788), new Rotation3d(new Quaternion(1.0, 0.0, 0.0, 0.0)));
 }
