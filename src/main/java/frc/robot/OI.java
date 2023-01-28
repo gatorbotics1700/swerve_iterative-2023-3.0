e474d6b3866a32efbbb9601f3c1317719c7aee95
MsBarnum-is-awesome <potternerd1218@gmail.com>
2023-01-27 17:33:23 -0800

    working on trigger code calls in robot.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
+
+    public double intakeOn, intakeOff;
+
     public static final XboxController m_controller = new XboxController(0);//buttons/co-driver controller
     public static final XboxController m_controller_two = new XboxController(1);//main driver/driving controller
 
@@ -20,13 +23,24 @@ public class OI {
     public static Button turnNorth = Button.kY;//button to turn to true north
 
 
-    public static Button intakeOn = Button.kA; //need to be coded for triggers rather than buttons
-    public static Button intakeOff = Button.kX; // need to figure out code for triggers
-    //public static Button outtake = Button. - need a to change intake to triggers so we can use buttons for outtake
-
+    public static Button outtake = Button.kA; 
+    
+    public void triggerPeriodic() {
+        intakeOn = triggerThreshold (m_controller.getLeftTriggerAxis());
+        intakeOff = triggerThreshold (m_controller.getRightTriggerAxis());
+    }
     /*two total xbox controllers, one for driver, one for co-driver. 4 buttons on each, 2 joysticks, 2 triggers, 2 bumbers, 2 additional buttons at the back of the contoller, and possibly 4 directional arrow keys. All can be programmed for different settings as desired.
      * buttons/joysticks to be coded:
      * - make joystick for driving robot
      * - outtake button
      */
+
+    public static double triggerThreshold(double tValue) {
+        if (Math.abs(tValue) < 0.1) { //change this value to be more specific if wanted
+            return 0;
+        } else {
+            return 1 * tValue;
+        }
+        //trigger axis is bound by the range [0,1] not [-1,1]
+    }
 }
