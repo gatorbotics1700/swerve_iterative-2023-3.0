a4af4143ddaa33ebf67c09ebe676ffc628a86180
dragonette13 <65913060+dragonette13@users.noreply.github.com>
2023-09-20 19:55:54 -0700

    created the easyTraj and easyTPath methods to shorten the trajectory and path creation process, and implemented them in Paths.java

--- src/main/java/frc/robot/autonomous/Paths.java
+++ src/main/java/frc/robot/autonomous/Paths.java
@@ -8,6 +8,10 @@ import javax.swing.plaf.nimbus.State;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.autonomous.AutonomousBaseMP;
+import frc.robot.autonomous.Trajectories;
+import edu.wpi.first.math.trajectory.Trajectory;
+
 
 
 public class Paths {
@@ -37,133 +41,35 @@ public class Paths {
         MP_HD3SCORER, 
         MP_TESTPATH;
     }
+    
+    // //all of the trajectories for each path go into an array
+    // //public static Trajectory[] mp = {Trajectories.uno, Trajectories.dos, Trajectories.tres, Trajectories.nada};
+    // public static Trajectory[] mp_hd3scorer = {Trajectories.oneHD3R, Trajectories.twoHD3R, Trajectories.threeHD3R, Trajectories.fourHD3R};
+    // public static Trajectory[] mp_testpath = {Trajectories.flowerOne, Trajectories.flowerTwo, Trajectories.flowerThree, Trajectories.flowerFour};
 
+    // //this way you don't need to type out the return new etc etc in each if statement
+    // public static AutonomousBaseMP autobasempgenerator(Trajectory[] t){
+    //     return new AutonomousBaseMP(t[0], t[1], t[2], t[3]);
+    // }
+    // public static double[] mp = {};
+
+
+    //shorter and hopefully easier to use than the previous version
     public static AutonomousBase constructAuto(AUTO_OPTIONS selectedAuto){
-        if(selectedAuto == AUTO_OPTIONS.TESTPATH){
-            return new AutonomousBasePD(
-                new Pose2d(0.0, 0.0, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                new StateWithCoordinate(AutoStates.FIRST),
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(5 * mpi, -40 * mpi, new Rotation2d(Math.toRadians(180)))), 
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(40 * mpi, 0, new Rotation2d(0))), 
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(0, 30 * mpi, new Rotation2d(0))), 
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(40 * mpi, 30 * mpi, new Rotation2d(0))), 
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(0, 0, new Rotation2d(0))), 
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(20 * mpi, 20 * mpi, new Rotation2d(0))),
-                new StateWithCoordinate(AutoStates.STOP)
-
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.NOGO){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X, 0.0, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                new StateWithCoordinate(AutoStates.FIRST),
-                new StateWithCoordinate(AutoStates.LOWNODE),
-                new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.HDLEAVEB){
-            return new AutonomousBasePD( 
-                new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                    new StateWithCoordinate(AutoStates.FIRST),
-                    new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),//278.95
-                    new StateWithCoordinate(AutoStates.STOP) 
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.HBLEAVEB){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                new StateWithCoordinate(AutoStates.FIRST),
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.LOWHDPLACELEAVEB){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                new StateWithCoordinate(AutoStates.FIRST),
-                new StateWithCoordinate(AutoStates.LOWNODE),
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d((225 + 30) * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.MIDHDPLACELEAVEB){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                new StateWithCoordinate(AutoStates.FIRST),
-                new StateWithCoordinate(AutoStates.MIDNODE),
-                new StateWithCoordinate(AutoStates.DRIVE, new Pose2d((225 + 30) * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.LOWHBPLACELEAVEB){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                    new StateWithCoordinate(AutoStates.FIRST),
-                    new StateWithCoordinate(AutoStates.LOWNODE),
-                    new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                    new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.MIDHBPLACELEAVEB){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                    new StateWithCoordinate(AutoStates.FIRST),
-                    new StateWithCoordinate(AutoStates.MIDNODE),
-                    new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                    new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.LOW_OVER_ENGAGE){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HD_Y_B * mpi + 4*mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                    new StateWithCoordinate(AutoStates.FIRST),
-                    new StateWithCoordinate(AutoStates.LOWNODE),
-                    new StateWithCoordinate(AutoStates.FASTDRIVE, new Pose2d(265 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                    new StateWithCoordinate(AutoStates.ENGAGE),
-                    new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if (selectedAuto == AUTO_OPTIONS.MID_OVER_ENGAGE){
-            return new AutonomousBasePD(
-                new Pose2d(STARTING_X * mpi, HD_Y_B * mpi + 4*mpi, new Rotation2d(Math.toRadians(180.0))), 
-                new StateWithCoordinate[]{
-                    new StateWithCoordinate(AutoStates.FIRST),
-                    new StateWithCoordinate(AutoStates.MIDNODE),
-                    new StateWithCoordinate(AutoStates.FASTDRIVE, new Pose2d(265 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
-                    new StateWithCoordinate(AutoStates.ENGAGE),
-                    new StateWithCoordinate(AutoStates.STOP)
-                }
-            );
-        } else if(selectedAuto == AUTO_OPTIONS.MP){
-            return new AutonomousBaseMP(
-                Trajectories.uno, 
-                Trajectories.dos,
-                Trajectories.tres,
-                Trajectories.nada
-            ); 
-        } else if(selectedAuto == AUTO_OPTIONS.MP_HD3SCORER){
-            return new AutonomousBaseMP(
-                Trajectories.oneHD3R, 
-                Trajectories.twoHD3R,
-                Trajectories.threeHD3R,
-                Trajectories.fourHD3R
-            ); 
-        } else if(selectedAuto == AUTO_OPTIONS.MP_TESTPATH){
-            return new AutonomousBaseMP(
-                Trajectories.flowerOne, 
-                Trajectories.flowerTwo,
-                Trajectories.flowerThree,
-                Trajectories.flowerFour
-            ); 
+        if(selectedAuto == AUTO_OPTIONS.MP){
+            //this looks like it might be in meters, but it should be in inches
+            return Trajectories.easyTPath(0.0, 0.0, 0.0, 2.0, 0.0, 90.0, 1.0, 0.0, 1.2, 0.25, 1.4, 0.0,
+            0.0, 0.0, 0.0, 1.5, 2.3, 0.0, 0.2, 0.5, 0.9, 1.5, 1.5, 2.0,
+            0.0, 0.0, 0.0, 2.0, -1.0, 0.0, 0.2, -0.4, 0.9, -0.6, 1.5, -0.8,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        
+        }else if(selectedAuto == AUTO_OPTIONS.MP_HD3SCORER){
+            return Trajectories.easyTPath(595.614, 20.19, 180, 372.684, 37.193, 0, 520.0, 25.2, 445.0, 30.2, 400.0, 33.19,
+            372.60, 37.193, 0.0, 595.461, 43.068, 180.0, 400.0, 33.19, 445.0, 30.2, 520.0, 25.2,
+            595.461, 43.068, 180.0, 372.606, 85.622, 0.0, 525.0, 43.5, 454.199, 45.934, 400.0, 55.0,
+            372.606, 85.622, 0.0, 595.529, 66.117, 180.0, 400.0, 55.0, 454.199, 45.934, 525.0, 43.5);
+        //and then add more else ifs for the other auto options
+    
         }else if(selectedAuto== AUTO_OPTIONS.LOWTIMEDENGAGED){
             return new AutonomousBaseEngage(1);
         } else if (selectedAuto== AUTO_OPTIONS.MIDTIMEDENGAGED){
@@ -174,6 +80,113 @@ public class Paths {
         else {
             return new AutonomousBaseTimed();
         }
+        // if(selectedAuto == AUTO_OPTIONS.TESTPATH){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(0.0, 0.0, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //         new StateWithCoordinate(AutoStates.FIRST),
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(5 * mpi, -40 * mpi, new Rotation2d(Math.toRadians(180)))), 
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(40 * mpi, 0, new Rotation2d(0))), 
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(0, 30 * mpi, new Rotation2d(0))), 
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(40 * mpi, 30 * mpi, new Rotation2d(0))), 
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(0, 0, new Rotation2d(0))), 
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(20 * mpi, 20 * mpi, new Rotation2d(0))),
+        //         new StateWithCoordinate(AutoStates.STOP)
+
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.NOGO){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X, 0.0, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //         new StateWithCoordinate(AutoStates.FIRST),
+        //         new StateWithCoordinate(AutoStates.LOWNODE),
+        //         new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.HDLEAVEB || selectedAuto == AUTO_OPTIONS.HBLEAVEB){
+        //     return new AutonomousBasePD( 
+        //         new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //             new StateWithCoordinate(AutoStates.FIRST),
+        //             new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),//278.95
+        //             new StateWithCoordinate(AutoStates.STOP) 
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.HBLEAVEB){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //         new StateWithCoordinate(AutoStates.FIRST),
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //         new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.LOWHDPLACELEAVEB){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //         new StateWithCoordinate(AutoStates.FIRST),
+        //         new StateWithCoordinate(AutoStates.LOWNODE),
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d((225 + 30) * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //         new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.MIDHDPLACELEAVEB){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //         new StateWithCoordinate(AutoStates.FIRST),
+        //         new StateWithCoordinate(AutoStates.MIDNODE),
+        //         new StateWithCoordinate(AutoStates.DRIVE, new Pose2d((225 + 30) * mpi, HB_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //         new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.LOWHBPLACELEAVEB){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //             new StateWithCoordinate(AutoStates.FIRST),
+        //             new StateWithCoordinate(AutoStates.LOWNODE),
+        //             new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //             new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.MIDHBPLACELEAVEB){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //             new StateWithCoordinate(AutoStates.FIRST),
+        //             new StateWithCoordinate(AutoStates.MIDNODE),
+        //             new StateWithCoordinate(AutoStates.DRIVE, new Pose2d(225 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //             new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.LOW_OVER_ENGAGE){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HD_Y_B * mpi + 4*mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //             new StateWithCoordinate(AutoStates.FIRST),
+        //             new StateWithCoordinate(AutoStates.LOWNODE),
+        //             new StateWithCoordinate(AutoStates.FASTDRIVE, new Pose2d(265 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //             new StateWithCoordinate(AutoStates.ENGAGE),
+        //             new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+        // } else if (selectedAuto == AUTO_OPTIONS.MID_OVER_ENGAGE){
+        //     return new AutonomousBasePD(
+        //         new Pose2d(STARTING_X * mpi, HD_Y_B * mpi + 4*mpi, new Rotation2d(Math.toRadians(180.0))), 
+        //         new StateWithCoordinate[]{
+        //             new StateWithCoordinate(AutoStates.FIRST),
+        //             new StateWithCoordinate(AutoStates.MIDNODE),
+        //             new StateWithCoordinate(AutoStates.FASTDRIVE, new Pose2d(265 * mpi, HD_Y_B * mpi, new Rotation2d(Math.toRadians(180.0)))),
+        //             new StateWithCoordinate(AutoStates.ENGAGE),
+        //             new StateWithCoordinate(AutoStates.STOP)
+        //         }
+        //     );
+       // } else if(selectedAuto == AUTO_OPTIONS.MP){
+        
+        
 
         /*public static AutonomousBasePD HDIntakeEngage = new AutonomousBasePD(
             new Pose2d(68.95 * mpi, 20.19 * mpi, new Rotation2d(Math.toRadians(180.0))), 
--- src/main/java/frc/robot/autonomous/Trajectories.java
+++ src/main/java/frc/robot/autonomous/Trajectories.java
@@ -138,47 +138,69 @@ public class Trajectories{
         System.out.println("End config pose: " + Robot.m_drivetrainSubsystem.getMPoseX());
         return trajectory;
     }
+
+    //the point of these two methods is to eliminate the redundancy of typing out all of the Pose2ds everytime you make a new trajectory
+    //hopefully they will shorten the code and make it easier to debug
+    //s is for start, e is for end, and wp is for waypoint
+    public static Trajectory easyTraj(double sx, double sy, double sr, double ex, double ey, double er, double wp1x, double wp1y, double wp2x, double wp2y, double wp3x, double wp3y){
+        return generateTrajectory(
+            new Pose2d(sx * mpi, sy * mpi, new Rotation2d(Math.toRadians(sr))),
+            new Pose2d(ex * mpi, ey * mpi, new Rotation2d(Math.toRadians(er))),
+            new Translation2d(wp1x * mpi, wp1y * mpi), 
+            new Translation2d(wp2x * mpi, wp2y * mpi),
+            new Translation2d(wp3x * mpi, wp3y * mpi)
+        );
+    }
+    //it's a ton of variables instead of an array because this way when you type in the values when you call the method it tells you which value is which
+    public static AutonomousBaseMP easyTPath(double sx1, double sy1, double sr1, double ex1, double ey1, double er1, double wp1x1, double wp1y1, double wp2x1, double wp2y1, double wp3x1, double wp3y1,
+    double sx2, double sy2, double sr2, double ex2, double ey2, double er2, double wp1x2, double wp1y2, double wp2x2, double wp2y2, double wp3x2, double wp3y2, 
+    double sx3, double sy3, double sr3, double ex3, double ey3, double er3, double wp1x3, double wp1y3, double wp2x3, double wp2y3, double wp3x3, double wp3y3,
+    double sx4, double sy4, double sr4, double ex4, double ey4, double er4, double wp1x4, double wp1y4, double wp2x4, double wp2y4, double wp3x4, double wp3y4){
+        Trajectory traj1 = easyTraj(sx1, sy1, sr1, ex1, ey1, er1, wp1x1, wp1y1, wp2x1, wp2y1, wp3x1, wp3y1);
+        Trajectory traj2 = easyTraj(sx2, sy2, sr2, ex2, ey2, er2, wp1x2, wp1y2, wp2x2, wp2y2, wp3x2, wp3y2);
+        Trajectory traj3 = easyTraj(sx3, sy3, sr3, ex3, ey3, er3, wp1x3, wp1y3, wp2x3, wp2y3, wp3x3, wp3y3);
+        Trajectory traj4 = easyTraj(sx4, sy4, sr4, ex4, ey4, er4, wp1x4, wp1y4, wp2x4, wp2y4, wp3x4, wp3y4);
+        return new AutonomousBaseMP(
+            traj1,
+            traj2,
+            traj3,
+            traj4
+        );
+    }
     //the point of the method is to not have to type out new pose2d etc etc every time you want a trajectory
-    //s stands for start (as in the starting pose2d), e is for end, and wp1,2, and 3 are waypoints
-    // public static Trajectory easyTrajectory(double sx, double sy, double sr, double ex, double ey, double er, double wp1x, double wp1y, double wp2x, double wp2y, double wp3x, double wp3y){
+    // //the order of the variables in the array is as follows: starting coords(xyr), end coords, and three waypoint coords(xy)
+    // public static Trajectory easyTrajectory(double[] coords){
     //     Trajectory traj = generateTrajectory(
-    //         new Pose2d(sx * mpi, sy * mpi, new Rotation2d(Math.toRadians(sr))),
-    //         new Pose2d(ex * mpi, ey * mpi, new Rotation2d(Math.toRadians(er))),
-    //         new Translation2d(wp1x * mpi, wp1y * mpi), 
-    //         new Translation2d(wp2x * mpi, wp2y * mpi),
-    //         new Translation2d(wp3x * mpi, wp3y * mpi)
+    //         new Pose2d(coords[0] * mpi, coords[1] * mpi, new Rotation2d(Math.toRadians(coords[2]))),
+    //         new Pose2d(coords[3] * mpi, coords[4] * mpi, new Rotation2d(Math.toRadians(coords[5]))),
+    //         new Translation2d(coords[6] * mpi, coords[7] * mpi), 
+    //         new Translation2d(coords[8] * mpi, coords[9] * mpi),
+    //         new Translation2d(coords[10] * mpi, coords[10] * mpi)
     //     );
     //     return traj;
-    // };
-    //new version with array
-    public static Trajectory easyTrajectory(double[] coords){
-        Trajectory traj = generateTrajectory(
-            new Pose2d(coords[0] * mpi, coords[1] * mpi, new Rotation2d(Math.toRadians(coords[2]))),
-            new Pose2d(coords[3] * mpi, coords[4] * mpi, new Rotation2d(Math.toRadians(coords[5]))),
-            new Translation2d(coords[6] * mpi, coords[7] * mpi), 
-            new Translation2d(coords[8] * mpi, coords[9] * mpi),
-            new Translation2d(coords[10] * mpi, coords[10] * mpi)
-        );
-        return traj;
-    }
-    //avery says you need to do it this way, you can't just call easyTrajectory
-    static double[] ntc = {2,3,4,5,6,7,8,9,10,11,12};
-    public static Trajectory ntrajectory = easyTrajectory(ntc);
-    //public static Trajectory ntrajectory = easyTrajectory(1,2,3,4,5,6,7,8,9,10,11,12);
-
-    public static AutonomousBaseMP easyTPath(double[] tp1, double[] tp2, double[] tp3, double[] tp4){
-        Trajectory trajectory1 = easyTrajectory(tp1);
-        Trajectory trajectory2 = easyTrajectory(tp2);
-        Trajectory trajectory3 = easyTrajectory(tp3);
-        Trajectory trajectory4 = easyTrajectory(tp4);
+    // }
+    
+    // static double[] ntc = {2,3,4,5,6,7,8,9,10,11,12};
+    // //avery says you need to do it this way, you can't just call easyTrajectory
+    // public static Trajectory ntrajectory = easyTrajectory(ntc);
+    // //public static Trajectory ntrajectory = easyTrajectory(1,2,3,4,5,6,7,8,9,10,11,12);
+    
+    // //TPath stands for trajectory path, rename it if that isn't intuitive
+    // public static AutonomousBaseMP easyTPath(double[] tp1, double[] tp2, double[] tp3, double[] tp4){
+    //     Trajectory trajectory1 = easyTrajectory(tp1);
+    //     Trajectory trajectory2 = easyTrajectory(tp2);
+    //     Trajectory trajectory3 = easyTrajectory(tp3);
+    //     Trajectory trajectory4 = easyTrajectory(tp4);
        
-        return new AutonomousBaseMP(
-            trajectory1,
-            trajectory2,
-            trajectory3,
-            trajectory4
-        );
-    }
+    //     return new AutonomousBaseMP(
+    //         trajectory1,
+    //         trajectory2,
+    //         trajectory3,
+    //         trajectory4
+    //     );
+    // }
+
+    
     
 
 
