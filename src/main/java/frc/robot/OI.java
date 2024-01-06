737e41c46d8e8baf03a8240613f7078aa59ffe23
Gatorbotics <gatorbotics1700@gmail.com>
2024-01-06 14:38:03 -0800

    fixed CAN IDs

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -17,23 +17,13 @@ public class OI {
 
     public static Button outtake = Button.kA; 
     
-    public void triggerPeriodic() {
-        intakeOn = triggerThreshold (m_controller.getLeftTriggerAxis());
-        intakeOff = triggerThreshold (m_controller.getRightTriggerAxis());
-    }
+
     /*two total xbox controllers, one for driver, one for co-driver. 4 buttons on each, 2 joysticks, 2 triggers, 2 bumbers, 2 additional buttons at the back of the contoller, and possibly 4 directional arrow keys. All can be programmed for different settings as desired.
      * buttons/joysticks to be coded:
      * - make joystick for driving robot
      * - outtake button
      */
 
-    public static double triggerThreshold(double tValue) {
-        if (Math.abs(tValue) < 0.1) { //change this value to be more specific if wanted
-            return 0;
-        } else {
-            return 1 * tValue;
-        }
-        //trigger axis is bound by the range [0,1] not [-1,1]
-    }
+    
 }
 
